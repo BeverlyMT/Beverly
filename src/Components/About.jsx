6070f6f562a8e3b9080c4972a09b69c9ea3a0d54
BeverlyMT <130603765+BeverlyMT@users.noreply.github.com>
2023-12-29 01:36:51 -0600

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = " background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a computer science student . I like to learn new things every day.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,10 +32,10 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "",
+  "",
+  "",
+  "",
   "Graphic design",
 ];
 
@@ -45,7 +45,9 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "“Reading allows us to travel through time, touching the wisdom of our ancestors with our fingertips.”
+
+-Carl Sagan-";
 
 const About = () => {
   return (
