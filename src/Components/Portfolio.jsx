400599b26da093e7d6e84400c906456665beef5c
BeverlyMT <130603765+BeverlyMT@users.noreply.github.com>
2023-12-29 01:27:58 -0600

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/luca-bravo-XJXWbfSo2f0-unsplash.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "just code";
 
 /**
  * Project list
